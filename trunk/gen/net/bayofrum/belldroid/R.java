39e1c85069b1240027ce506b0813f9b362b35549
utisoft@gmail.com <utisoft@gmail.com@e386f765-1f9f-8ced-a92a-6f4c159de53f>
2014-04-27 17:30:01 +0000

    Now allows manual control
    Can introduce delay to make simulation realistic
    
    Can customise peal time

--- trunk/gen/net/bayofrum/belldroid/R.java
+++ trunk/gen/net/bayofrum/belldroid/R.java
@@ -8,6 +8,9 @@
 package net.bayofrum.belldroid;
 
 public final class R {
+    public static final class array {
+        public static final int bellsAvailable=0x7f050000;
+    }
     public static final class attr {
         /** <p>Must be a reference to another resource, in the form "<code>@[+][<i>package</i>:]<i>type</i>:<i>name</i></code>"
 or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>".
@@ -19,7 +22,7 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int buttonBarStyle=0x7f010000;
     }
     public static final class color {
-        public static final int black_overlay=0x7f040000;
+        public static final int black_overlay=0x7f060000;
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
@@ -28,33 +31,32 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f050000;
-        public static final int activity_vertical_margin=0x7f050001;
+        public static final int activity_horizontal_margin=0x7f070000;
+        public static final int activity_vertical_margin=0x7f070001;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f090004;
-        public static final int debug=0x7f090002;
-        public static final int mainTable=0x7f090003;
-        public static final int methodChooser=0x7f090001;
-        public static final int numberOfBells=0x7f090000;
+        public static final int action_settings=0x7f0b0002;
+        public static final int debug=0x7f0b0000;
+        public static final int mainTable=0x7f0b0001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
     }
     public static final class menu {
-        public static final int main=0x7f080000;
+        public static final int main=0x7f0a0000;
     }
     public static final class string {
-        public static final int action_settings=0x7f060004;
-        public static final int app_name=0x7f060000;
-        public static final int dummy_button=0x7f060001;
-        public static final int dummy_content=0x7f060002;
-        public static final int hello_world=0x7f060005;
-        public static final int timer_name=0x7f060006;
-        public static final int title_activity_main=0x7f060003;
+        public static final int action_settings=0x7f080004;
+        public static final int app_name=0x7f080000;
+        public static final int dummy_button=0x7f080001;
+        public static final int dummy_content=0x7f080002;
+        public static final int hello_world=0x7f080005;
+        public static final int number_of_bells=0x7f080007;
+        public static final int timer_name=0x7f080006;
+        public static final int title_activity_main=0x7f080003;
     }
     public static final class style {
         /** 
@@ -81,15 +83,18 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f070000;
+        public static final int AppBaseTheme=0x7f090000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f070001;
-        public static final int ButtonBar=0x7f070003;
-        public static final int ButtonBarButton=0x7f070004;
-        public static final int FullscreenActionBarStyle=0x7f070005;
-        public static final int FullscreenTheme=0x7f070002;
+        public static final int AppTheme=0x7f090001;
+        public static final int ButtonBar=0x7f090003;
+        public static final int ButtonBarButton=0x7f090004;
+        public static final int FullscreenActionBarStyle=0x7f090005;
+        public static final int FullscreenTheme=0x7f090002;
+    }
+    public static final class xml {
+        public static final int preferences=0x7f040000;
     }
     public static final class styleable {
         /** 
---
+++ trunk/src/net/bayofrum/belldroid/PreferencesActivity.java
@@ -0,0 +1,100 @@
+package net.bayofrum.belldroid;
+
+import java.util.ArrayList;
+
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
+import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceCategory;
+import android.preference.PreferenceManager;
+
+public class PreferencesActivity extends PreferenceActivity {
+
+	private SharedPreferences spref;
+	/* Totally unbelieveable.  You have to read this!
+	 * http://stackoverflow.com/questions/2542938/sharedpreferences-onsharedpreferencechangelistener-not-being-called-consistently
+	 */
+	private OnSharedPreferenceChangeListener prefchangelistener;
+	
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	    /* Might crash without next line ?? */
+//	    PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
+	    addPreferencesFromResource(R.xml.preferences);
+	    spref = PreferenceManager.getDefaultSharedPreferences(this);
+		prefchangelistener = new OnSharedPreferenceChangeListener() {
+			@Override
+			public void onSharedPreferenceChanged(SharedPreferences s,
+					String key) {
+				if (key.equals("number_of_bells")) {
+					String numBells = spref.getString("number_of_bells", "6");
+					set_method_prefs(numBells);
+					set_my_bell(numBells);
+				}
+			}
+		};
+		
+	    register_changelistener();
+	}
+	
+	@Override
+	public void onResume() {
+		super.onResume();
+		register_changelistener();
+	}
+	
+	private void register_changelistener() {
+		spref.registerOnSharedPreferenceChangeListener(prefchangelistener);
+		String numBells = spref.getString("number_of_bells", "6");
+		set_method_prefs(numBells);
+		set_my_bell(numBells);
+	}
+	
+	private void set_method_prefs(int numBells) {
+
+		ListPreference methodpref = 
+				(ListPreference)findPreference("selected_method");
+	    
+		ArrayList<CharSequence> names = new ArrayList<CharSequence>();
+		ArrayList<CharSequence> notation = new ArrayList<CharSequence>();
+	    
+	    for (CharSequence s[] : MainActivity.methods[numBells]) {
+	    	names.add(s[0]);
+	    	notation.add(s[1]);
+	    }
+	    
+	    methodpref.setEntries(names.toArray(new CharSequence[0]));
+	    methodpref.setEntryValues(notation.toArray(new CharSequence[0]));
+	    methodpref.setDefaultValue(methodpref.getEntries()[0]);
+	}
+
+	private void set_method_prefs(String s) {
+		set_method_prefs(Integer.parseInt(s));
+	}
+	
+	private void set_my_bell(int numBells) {
+
+		ListPreference myBell = 
+				(ListPreference)findPreference("my_bell");
+	    
+		ArrayList<String> entries = new ArrayList<String>();
+		
+	    for (int i = 0; i < numBells; i++) {
+	    	entries.add(String.valueOf(i+1));
+	    }
+	    
+	    entries.add("None");
+	    
+	    myBell.setEntries(entries.toArray(new CharSequence[0]));
+	    myBell.setEntryValues(entries.toArray(new CharSequence[0]));
+	    myBell.setDefaultValue("None");
+	}
+	
+	private void set_my_bell(String s) {
+		set_my_bell(Integer.parseInt(s));
+	}
+}
